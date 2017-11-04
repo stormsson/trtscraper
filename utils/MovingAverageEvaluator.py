90e26a319e76663dd929d233822702ed45a7b4cf
Camillo Camarda <ccamarda@Camardas-MacBook-Pro.fritz.box>
2017-11-04 11:04:39 +0100

    adfdsfgfdf

--- utils/MovingAverageEvaluator.py
+++ utils/MovingAverageEvaluator.py
@@ -8,36 +8,40 @@ class MovingAverageEvaluator:
         self.dbManager = dbManager
 
 
-    def run(self, avg_minutes,avg_field):
+    def run(self, avg_minutes,avg_field, field_to_save):
         docBuffer = []
         writeBuffer = []
 
-        for doc in self.dbManager.db["test_query_collection"].find({avg_field: {"$exists": False}}).sort('date',pymongo.ASCENDING):
+        for doc in self.dbManager.db["test_query_collection"].find({field_to_save: {"$exists": False}}).sort('date',pymongo.ASCENDING):
         # for doc in self.dbManager.fundCollection.find().sort('date',pymongo.ASCENDING):
-          docBuffer.append(doc)        # Add to buffer
-        print(doc)
-        exit()
+            docBuffer.append(doc)        # Add to buffer
 
-        # Filter buffer for expired
-        docBuffer = filter(lambda x: x['date'] >= (doc['date'] - timedelta(minutes=avg_minutes)), docBuffer)
 
-        writeBuffer.append({
-            "updateOne": {
-                "filter": { "_id": doc['_id'] },
-                "update": {
-                    "$set": {
-                        avg_field: reduce(lambda x,y: y['bid'] + x, docBuffer, 0) / len(list(docBuffer)),
+            # Filter buffer for expired
+            docBuffer = filter(lambda x: x['date'] >= (doc['date'] - timedelta(minutes=avg_minutes)), docBuffer)
+            for i in docBuffer:
+                print(i)
+
+
+            bufferLength = len(list(docBuffer)) if len(list(docBuffer)) else 1
+
+            writeBuffer.append({
+                "updateOne": {
+                    "filter": { "_id": doc['_id'] },
+                    "update": {
+                        "$set": {
+                            field_to_save: reduce(lambda x,y: y[avg_field] + x, docBuffer, 0) / bufferLength,
+                        }
                     }
                 }
-            }
-        })
+            })
 
-        # Write if buffer has enough to make a bulk write
-        if len(writeBuffer) > 10:
-            collection.bulk_write(writeBuffer);
-            writeBuffer = []
+            # Write if buffer has enough to make a bulk write
+            if len(writeBuffer) > 10:
+                collection.bulk_write(writeBuffer);
+                writeBuffer = []
 
-        # Clear any buffered writes
-        if len(writeBuffer) > 0:
-            collection.bulk_write(writeBuffer);
-            writeBuffer = []
+            # Clear any buffered writes
+            if len(writeBuffer) > 0:
+                collection.bulk_write(writeBuffer);
+                writeBuffer = []
