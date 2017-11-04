15ed24db833f1be95c4655e63bb48c8481f7071a
Camillo Camarda <ccamarda@Camardas-MacBook-Pro.fritz.box>
2017-11-04 11:15:57 +0100

    hgdfs

--- utils/MovingAverageEvaluator.py
+++ utils/MovingAverageEvaluator.py
@@ -2,6 +2,9 @@ from datetime import datetime, timedelta
 from functools import reduce
 import pymongo
 
+from pymongo import UpdateOne
+
+
 class MovingAverageEvaluator:
 
     def __init__(self, dbManager):
@@ -12,7 +15,9 @@ class MovingAverageEvaluator:
         docBuffer = []
         writeBuffer = []
 
-        for doc in self.dbManager.db["test_query_collection"].find({field_to_save: {"$exists": False}}).sort('date',pymongo.ASCENDING):
+        collection = self.dbManager.db["test_query_collection"]
+
+        for doc in collection.find({field_to_save: {"$exists": False}}).sort('date',pymongo.ASCENDING):
         # for doc in self.dbManager.fundCollection.find().sort('date',pymongo.ASCENDING):
             docBuffer.append(doc)        # Add to buffer
 
@@ -25,16 +30,24 @@ class MovingAverageEvaluator:
 
             bufferLength = len(list(docBuffer)) if len(list(docBuffer)) else 1
 
-            writeBuffer.append({
-                "updateOne": {
-                    "filter": { "_id": doc['_id'] },
-                    "update": {
-                        "$set": {
-                            field_to_save: reduce(lambda x,y: y[avg_field] + x, docBuffer, 0) / bufferLength,
+            writeBuffer.append(UpdateOne(
+                { "_id": doc['_id'] },
+                { "$set": {
+                        field_to_save: reduce(lambda x,y: y[avg_field] + x, docBuffer, 0) / bufferLength
                         }
-                    }
                 }
-            })
+            ))
+
+            # writeBuffer.append({
+            #     "updateOne": {
+            #         "filter": { "_id": doc['_id'] },
+            #         "update": {
+            #             "$set": {
+            #                 field_to_save: reduce(lambda x,y: y[avg_field] + x, docBuffer, 0) / bufferLength,
+            #             }
+            #         }
+            #     }
+            # })
 
             # Write if buffer has enough to make a bulk write
             if len(writeBuffer) > 10:
